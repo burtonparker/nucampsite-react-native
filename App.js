a7c1da06f4da39385eb4d9fa3961d19a72684b39
Burton Parker <burtonparker@gmail.com>
2022-01-18 12:05:53 -0600

    fuck you

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import React from 'react';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Fuck you :)</Text>
       <StatusBar style="auto" />
     </View>
   );
