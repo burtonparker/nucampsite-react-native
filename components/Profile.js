a6f2d9145b6b9d919e67f13b54c6620e580f819e
Burton Parker <burtonparker@gmail.com>
2022-01-22 14:33:26 -0600

    Profile sandbox from week 1 workshop after class

---
+++ components/Profile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text, ScrollView } from 'react-native';
+
+class Profile extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Hello World</Text>
+            </View>
+        );
+    }
+}
+
+export default Profile;
