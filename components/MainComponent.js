555a79737ee7f26c3854c1e23c449e99f8b2d3e3
Burton Parker <burtonparker@gmail.com>
2022-01-26 13:11:22 -0600

    Navigation Icons and Custom Side Drawer, part 3 - custom side drawer

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -5,14 +5,15 @@ import CampsiteInfo from './CampsiteInfoComponent';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
 import Constants from 'expo-constants';
-import { View, Platform, StyleSheet } from 'react-native'; // Platform for conditional code adjustments
+import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native'; // Platform for conditional code adjustments
 // the Main component is a central hub and will hold all of our navigators.
 import { createStackNavigator } from 'react-navigation-stack'; // one required argument, RouteConfigs object
 import { createAppContainer } from 'react-navigation';
-import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
 import { Icon } from 'react-native-elements';
 import ExpoStatusBar from 'expo-status-bar/build/ExpoStatusBar';
 import { StatusBar } from 'expo-status-bar';
+import SafeAreaView from 'react-native-safe-area-view';
 
 // set which components will be available to the stack
 const DirectoryNavigator = createStackNavigator(
@@ -126,6 +127,28 @@ const ContactNavigator = createStackNavigator(
     }
 );
 
+const CustomDrawerContentComponent = props => ( // recieves props as it's parameter and will return the view of our customized drawer
+    <ScrollView>
+        <SafeAreaView /* specific to iPhone X and up, accounts for rounded corners, notch. default drawer navigator layout handles this but because we're replacing this with our own custom drawer we have to add SafeAreaView */ 
+            style={styles.container}
+            forceInset={{top: 'always', horizontal: 'never'}}
+        >
+            <View style={styles.drawerHeader}>
+                <View style={{flex: 1}} /* 1/3 of the drawer header view */ >
+                    <Image
+                        source={require('./images/logo.png')}
+                        style={styles.drawerImage}
+                    />
+                </View>
+                <View style={{flex: 2}} /* 2/3 of the drawer header view */ >
+                    <Text style={styles.drawerHeaderText}>NuCamp</Text>
+                </View>
+            </View>
+            <DrawerItems {...props} /* show items in the side drawer, passed as props, spread syntax props to pass them, now we need to connect to DrawerNavigator */ /> 
+        </SafeAreaView>
+    </ScrollView>
+);
+
 // drawer navigator, this needs for it's first argument an object that contains the screens that will be in the drawer. pass the Navigator versions of the components so we get the stack navigator versions.
 
 const MainNavigator = createDrawerNavigator(
@@ -187,7 +210,8 @@ const MainNavigator = createDrawerNavigator(
     },
     // as always, this second argument is optional but the first is required
     {
-        drawerBackgroundColor: '#cec8ff'
+        drawerBackgroundColor: '#cec8ff',
+        contentComponent: CustomDrawerContentComponent // connecting our component to the DrawerNavigator, this tells it to use our custom drawer component to render the content of the side drawer, nice!
     }
 );
 
@@ -233,6 +257,27 @@ class Main extends Component {
 // end goal is to render the CampsiteInfo component below the directory whenever one of the directory items is clicked.
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    drawerHeader: {
+        backgroundColor: '#5637DD',
+        height: 140,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row'
+    },
+    drawerHeaderText: {
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    drawerImage: {
+        margin: 10,
+        height: 60,
+        width: 60
+    },
     stackIcon: {
         marginLeft: 10,
         color: '#fff',
